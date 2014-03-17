204cab8d35b1ef9ce81e039221f71ee03aa4c486
Tara Roys <tlroys@gmail.com>
2014-03-17 09:30:37 -0500

    Created new Rails app
    With the command rails new blog --database=postgresql
    
    This creates a rails blog with postgres as a database.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsRspecSkeleton::Application.config.secret_key_base = '9d848b7eaa4968fb734b9b67b2700b8f1230c4baabdc87b038260ed5dfe0c4754055de82aac60ab6d3e4a0e308fbd2c4d39cbd521921dd98e0851f5b84104aed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsRspecSkeleton::Application.config.session_store :cookie_store, key: '_RailsRspecSkeleton_session'
